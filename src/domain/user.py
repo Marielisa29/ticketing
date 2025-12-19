ec3f2fdc9ab7d48dd0adc143a212f7bf0873b3fe
Marielisa29 <marielisaslb@gmail.com>
2025-12-19 11:59:40 +0100

    TD1A : Add post_init at user

--- src/domain/user.py
+++ src/domain/user.py
@@ -7,3 +7,7 @@ class User:
     username: str
     is_agent: bool = False
     is_admin: bool = False
+
+    def __post_init__(self):
+        if not self.username:
+            raise ValueError("Ticket title cannot be empty.")
