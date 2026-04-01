18dda7e17b86a446606b1d582a6ed994353ed45b
Marielisa29 <marielisaslb@gmail.com>
2026-04-01 09:53:04 +0200

    TD4a : Bonus

--- src/main.py
+++ src/main.py
@@ -13,16 +13,21 @@ Les cas d'usage ne voient que les interfaces (ports).
 from fastapi import FastAPI
 
 from src.adapters.api.ticket_router import router as ticket_router
+from src.adapters.api.user_router import router as user_router
 from src.adapters.db.ticket_repository_inmemory import InMemoryTicketRepository
+from src.adapters.db.user_repository_inmemory import InMemoryUserRepository
 from src.application.usecases.assign_ticket import AssignTicketUseCase
 from src.application.usecases.create_ticket import CreateTicketUseCase
+from src.application.usecases.create_user import CreateUserUseCase
 from src.application.usecases.list_ticket import ListTicketsUseCase
+from src.application.usecases.list_user import ListUsersUseCase
 
 app = FastAPI(title="Ticketing Starter")
 
 # --- Configuration de l'injection de dépendances ---
 # Création des instances d'adaptateurs (instance unique partagée entre les requêtes)
 ticket_repository = InMemoryTicketRepository()
+user_repository = InMemoryUserRepository()
 
 
 # Fonctions factory pour les cas d'usage (FastAPI les appellera via Depends)
@@ -36,16 +41,25 @@ def get_create_ticket_usecase() -> CreateTicketUseCase:
     return CreateTicketUseCase(ticket_repository)
 
 
-def get_assign_ticket_usecase() -> AssignTicketUseCase:
-    return AssignTicketUseCase(ticket_repository)
-
-
 def get_list_tickets_usecase() -> ListTicketsUseCase:
     return ListTicketsUseCase(ticket_repository)
 
 
+def get_assign_ticket_usecase() -> AssignTicketUseCase:
+    return AssignTicketUseCase(ticket_repository)
+
+
+def get_create_user_usecase() -> CreateUserUseCase:
+    return CreateUserUseCase(user_repository)
+
+
+def get_list_users_usecase() -> ListUsersUseCase:
+    return ListUsersUseCase(user_repository)
+
+
 # --- Routes ---
 app.include_router(ticket_router)
+app.include_router(user_router)
 
 
 @app.get("/")
